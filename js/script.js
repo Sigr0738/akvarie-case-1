073266f23309fe155d5c6a69d9f2b25728c544b1
Sigr0738 <sigridskovbak@gmail.com>
2026-03-16 10:45:17 +0100

    sigrids first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Spil parameterne 
+let score = 0;
+let maxScore = 100;
+
+let = updateProgress
+
+
+
+function removeTrash(trashElement){
+ trashElement.remove();
+ score += 20;
+ updateProgress();
+}
